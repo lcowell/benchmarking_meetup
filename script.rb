c8cd4e6c75f86d8d7272a52313fe2b831d3ad8c9
Luke Cowell <lcowell@gmail.com>
2011-11-11 08:26:43 -0800

    added hash search method added benchmarks

--- script.rb
+++ script.rb
@@ -1,6 +1,18 @@
 require './word_finder'
+require 'benchmark'
 
 WordFinder.search_and_profile(:search1)
-WordFinder.search_and_profile(:search2)
 WordFinder.search_and_profile(:search3)
+WordFinder.search_and_profile(:search2)
 WordFinder.search_and_profile(:search4)
+WordFinder.search_and_profile(:search5)
+
+debug = false
+
+Benchmark.bmbm do |x|
+  [5,10,100].each do |count|
+    %w(search2 search3 search4 search5).each do |search_method|
+      x.report("#{search_method} @ #{count}") { WordFinder.search_and_profile("#{search_method}", count, debug) }
+    end
+  end
+end
--- word_finder.rb
+++ word_finder.rb
@@ -3,13 +3,23 @@ require 'ruby-prof'
 require 'strscan'
 
 class WordFinder
-  def self.search_and_profile(method)
-    terms = %w(apple zebra zaphod manatee)
+  def self.search_and_profile(method, term_count = 5, debug = true)
+    if class_variable_defined?(:@@terms)
+      if @@terms.length != term_count
+        puts "regenerating terms" if debug
+        @@terms = random_terms(term_count)
+      end
+    else
+      puts "generating terms" if debug
+      @@terms = random_terms(term_count)
+    end
+    
     result = RubyProf.profile do
       f = new
-      puts "===starting #{method}==="
-      terms.each do |term|
-        puts "#{term}: #{f.send(method, term)}"
+      puts "===starting #{method}===" if debug
+      @@terms.each do |term|
+        result = f.send(method, term)
+        puts "#{term}: #{result}" if debug
       end
     end
 
@@ -19,195 +29,51 @@ class WordFinder
     printer.print(File.new("profiles/#{method}.txt", "w+"))
   end
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  # def search1(term)
-  #   result = nil
-  #   File.read(filename).split("\n").each do |line|
-  #     if line =~ %r(^#{term}$)
-  #       result = line
-  #     end
-  #   end
-  #   !!result
-  # end
-  # 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  # def search2(term)
-  #   !File.read(filename).scan(/^#{term}$/).empty?
-  # end
-  # 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  # def search3(term)
-  #   @data ||= File.read(filename)
-  #   !@data.scan(/^#{term}$/).empty?
-  # end
-  # 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  # def search4(term)
-  #   @data ||= File.read(filename)
-  #   s = StringScanner.new(@data)
-  #   !!s.scan_until(/^#{term}$/)
-  # end
+  def search1(term)
+    result = nil
+    load_data.split("\n").each do |line|
+      if line =~ %r(^#{term}$)
+        result = line
+      end
+    end
+    !!result
+  end
+  
+  def search2(term)
+    !load_data.scan(/^#{term}$/).empty?
+  end
+  
+  def search3(term)
+    @data ||= load_data
+    !@data.scan(/^#{term}$/).empty?
+  end
+  
+  def search4(term)
+    @data ||= load_data
+    s = StringScanner.new(@data)
+    !!s.scan_until(/^#{term}$/)
+  end
+  
+  def search5(term)
+    raise "error"
+    unless @data_hash
+      puts "generating hash"
+      @data_hash = load_data.split("\n").inject({}) {|acc,word| acc[word.to_s] = true; acc  }
+    end
+    !!@data_hash[term]
+  end
   
   private
   
-  def filename
-    "data.dat"
+  def self.load_data
+    File.read("data.dat")
+  end
+  
+  def load_data
+    self.class.load_data
+  end
+  
+  def self.random_terms(term_count)
+    load_data.split("\n").sample(term_count)
   end
 end
